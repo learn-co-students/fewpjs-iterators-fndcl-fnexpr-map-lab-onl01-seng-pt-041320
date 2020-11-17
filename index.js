fdf6797e3e9d8113dd225144bef7725ab01d018b
christinezosche <christine.zosche@gmail.com>
2020-11-17 13:04:17 -0800

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() { return tutorials.map(function(tutorial) {
+
+  let words = tutorial.split(" ")
+  let newWords = words.map(function(word) {
+    
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  })
+  let sentence = newWords.join(" ")
+  return sentence 
+  });
 }
